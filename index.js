3fbbff5644ed91463d54f362052aa1bfa95e0d07
omar124345 <70497377+omar124345@users.noreply.github.com>
2021-01-25 17:15:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
   client.user.setActivity(`بيوي و حكسر بس`);
   client.user.setStatus("idle");
 });
-const owners = ["455108784579149835", "527826654660132890" , "762002753878753310" ]
+const owners = ["455108784579149835", "527826654660132890" , "761716460288344064" , ]
 var prefix = "-"; 
 
 client.on("message", async message => {
